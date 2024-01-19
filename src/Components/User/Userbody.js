3da576758fddf937287a32e678dc62b721997554
vishal112222 <vishumark45@gmail.com>
2024-01-19 17:52:50 +0530

    first commit

--- src/Components/User/Userbody.js
+++ src/Components/User/Userbody.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Crtacc from '../Form/Crtacc';
 import DropDown from '../Dropdown/Dropdown';
-import Email from '../emailForm/Email';
+import Email from '../Emailform/email';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
